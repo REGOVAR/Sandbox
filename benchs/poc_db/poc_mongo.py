ee6fc5c6928a0bab45df0e4fe7dd3ab92c499e94
dridk <sacha@labsquare.org>
2016-07-25 23:16:07 +0200

    comment mongo test

--- benchs/poc_db/poc_mongo.py
+++ benchs/poc_db/poc_mongo.py
@@ -7,8 +7,8 @@ from mongoengine import *
 import random
 
 # ================= MONGOENGINE SCHEMA =============================
-
-
+# On défini la collection Variant ! Je pense que unique et meta font la meme choses.. 
+# Mais on ne sais jamais .. 
 class Variant(Document):
 	bin 	= IntField(required = True, default = -1)
 	chrom 	= StringField(max_length = 50, unique_with =('pos','ref','alt'))
@@ -21,29 +21,22 @@ class Variant(Document):
 	}
 
 
+# Ce n'est pas une collections , mais un sous documents qu'on retrouve dans Samples ! 
 class Genotype(EmbeddedDocument):
 	variant  = ReferenceField(Variant)
 	infos    = DictField()
 
-
+# Chaques samples contients la listes de ses variants associées à des infos 
 class Sample(Document):
 	name      = StringField(max_length=255)
 	genotypes =  ListField(EmbeddedDocumentField(Genotype))
 
 
 # ================= SCRIPTS =============================
-
-DBNAME   	= "regovar-dev"
-USER     	= "regovar"
-HOST     	= "localhost"
-PASSWORD 	= "regovar" 
-
-#========================================================
 def import_vcf(filename):
 
 	print("import vcf ...")
-	# On crée le schema.. Si ca existe deja on supprime tout . Le schema est dans le namespace regovar. Donc tinquiete , ca va pas effacer tes tables. 
-
+	# on Compte le nombre de lignes .. 
 	print("count records ...")
 	count = sum([1 for i in VariantFile(filename)]) 
 	print(count,"records found")
@@ -51,59 +44,53 @@ def import_vcf(filename):
 	bar     = Bar('Importing in cache... ', max = count, suffix='%(percent)d%%')
 	bcf_in  = VariantFile(filename) 
 
+	# Permet de garder en cache que les variants uniques 
 	uniques = set()
 
 
-	# On loop sur les variants 
-	for rec in bcf_in.fetch():
+	# On loop sur les variants et on met tout en mémoire
+	for rec in bcf_in.fetch(): 
 		chrom = str(rec.chrom)
 		pos   = int(rec.pos) 
 		ref   = str(rec.ref)
 		alt   = str(rec.alts[0])
 
 		key = (chrom,pos,ref,alt) 
-
 		uniques.add(key)
 		bar.next()
 
 	bar.finish()
 	
 
-	# Create objects 
 	bar     = Bar('Create objects... ', max = len(uniques), suffix='%(percent)d%%')
-
+	
+	# On va créer les objects de l'ORM de mongo et les mettres dans bulks
 	bulks = []
 	for key in uniques:
 		bulks.append(Variant(chrom = key[0], pos = key[1], ref = key[2], alt = key[3]))
 		bar.next()
-
 	bar.finish()
 
+	# On injecte les datas dans mongo
 	print("Save objects")
 	out = Variant.objects.insert(bulks)
 	
 
-
+	# Exemple pour sauvegarder les samples ! Je n'ai pas fini ... donc j'ai mis des variants au hasard
 	for name in rec.samples:
 
 		sample = Sample(name = name)
 		sample.genotypes.append(Genotype(variant = random.choice(out), infos = {"DP":45, "geno":-1}))
+		sample.genotypes.append(Genotype(variant = random.choice(out), infos = {"DP":42, "geno":1}))
+		sample.genotypes.append(Genotype(variant = random.choice(out), infos = {"DP":5, "geno":-1}))
+		sample.genotypes.append(Genotype(variant = random.choice(out), infos = {"DP":4, "geno":1}))
+		sample.genotypes.append(Genotype(variant = random.choice(out), infos = {"DP":425, "geno":1}))
+		sample.genotypes.append(Genotype(variant = random.choice(out), infos = {"DP":452, "geno":-1}))
+
 		sample.save()
 
 
-		# bar = Bar('Importing sample: {} '.format(name), max = len(samples_vars[name]), suffix='%(percent)d%%')
 
-		# curr.execute("INSERT INTO regovar.samples (name,description,file_id) VALUES (%s,%s,%s) RETURNING id", (name,"",file_id))   # it's a tupple ( x,)
-		# sample_id = curr.fetchone()[0]
-		# # Insert dans sample_variants
-
-		# for var_id in samples_vars[name]:
-		# 	curr.execute("INSERT INTO regovar.sample_has_variants (sample_id,variant_id,genotype) VALUES (%s,%s,%s) RETURNING id", (sample_id,var_id,-1))   # it's a tupple ( x,)
-		# 	bar.next()
-
-		# bar.finish()
-		# conn.commit()
-	# # On importe les samples 
 
 
 #=========== MAIN =======================
